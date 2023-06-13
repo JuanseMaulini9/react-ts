5c0bee59f8767b9e8b31aad500b2f1b1f51d19fd
Juanse <juansesosa45@gmail.com>
2023-06-13 16:41:45 -0300

    Typed in React

---
+++ components/Randomfox.tsx
@@ -0,0 +1,7 @@
+type Props = { image: string };
+
+export const RandomFox = ({image}: Props): JSX.Element => {
+  return (
+    <img width={320} height="auto" className="rounded" src={image} />
+  );
+};
